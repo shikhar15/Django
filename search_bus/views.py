14d32088e6e90f1c96e22909a35aff876839bfb5
shikhar15 <singhshikhar1515@gmail.com>
2020-07-10 02:02:55 +0530

    Bus_portal second version

--- search_bus/views.py
+++ search_bus/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import details
 # Create your views here.
 
@@ -7,6 +8,18 @@ def search_bus(request):
     return render(request, 'search_bus.html')
 
 def search_result(request):
-
-    bus_records = details.objects.filter(origin_place='Jaipur')
-    return render(request, 'search_result.html',{'bus_records':bus_records})
+    if request.method == 'POST':
+        start_place = request.POST['start_place']
+        end_place = request.POST['end_place']
+        date = request.POST['date_journey']
+        
+        if details.objects.filter(origin_place=start_place , destination_place=end_place , journey_date=date).exists():
+            bus_records = details.objects.filter(origin_place=start_place , destination_place=end_place , journey_date=date)
+            return render(request, 'search_result.html',{'bus_records':bus_records})
+        else:
+            messages.info(request,'NO BUS FOUND')
+            return redirect('search_bus')
+               
+    
+    else:
+        return redirect('search_bus')
